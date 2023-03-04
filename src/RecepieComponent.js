cff9a6d7ccfe0abe66215fa76e2e594dadc88a08
Kate <bozhko2906872020@gmail.com>
2023-03-04 16:22:00 +0300

    last

--- src/RecepieComponent.js
+++ src/RecepieComponent.js
@@ -10,7 +10,7 @@ function RecepieComponent({label, image, calories, ingredients, dish}) {
        <ul className="list">
           {ingredients.map((element,index) => (
           <li key={index}>
-             <img src="https://img.freepik.com/free-vector/green-eco-loop-leaf-check-mark_78370-658.jpg?w=740&t=st=1677316023~exp=1677316623~hmac=65cfc0081ce55c62e7cfbe102ec5f6ba99013901bdb2bd546869af591980009d" width="50px"/>{element}
+             <img src="https://img.freepik.com/free-vector/green-eco-loop-leaf-check-mark_78370-658.jpg?w=740&t=st=1677316023~exp=1677316623~hmac=65cfc0081ce55c62e7cfbe102ec5f6ba99013901bdb2bd546869af591980009d" width="50px" alt="tick"/>{element}
           </li>
           ))}
        </ul>
